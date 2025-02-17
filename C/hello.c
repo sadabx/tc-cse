d91724d131c948bc91e0d198b383a784b940a238
Sadab <127580924+sadabx@users.noreply.github.com>
2025-02-17 23:27:40 +0600

    Update largest-number.c

--- C/hello.c
+++ C/hello.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("Enter three value :");
+	scanf("%d%d%d", &a, &b, &c);
 
+	if (a > b)
+	{
+		if (a > c)
+		{
+			printf("The largest number is : %i", a);
+		}
+		else
+		{
+			printf("The largest number is : %i", c);
+		}
+	}
+	else
+	{
+		if (b > c)
+		{
+			printf("The largest value is : %i", b);
+		}
+		else
+		{
+			printf("The largest number is : %i", c);
+		}
+	}
+
+	return 0;
+}
