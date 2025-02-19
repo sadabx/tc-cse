7e54fae66abc9a71d0aabb80794d7c72064811e6
Sadab <127580924+sadabx@users.noreply.github.com>
2025-02-19 22:57:07 +0600

    Update if-else-ladder.c

--- if-else-ladder.c
+++ if-else-ladder.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int num;
+
+    // Input from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // If-Else Ladder
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else if (num < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
